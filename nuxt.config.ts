1172a678fd2a76c6c146b012ffeb342e21ce259f
xingxingmofashu <xingxingmofashu@outlook.com>
2025-06-13 13:36:11 +0800

    feat: add  internationalization

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,8 +10,16 @@ export default defineNuxtConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-  shadcn:{
+  shadcn: {
     componentDir: './app/components/ui'
+  },
+  i18n: {
+    defaultLocale: 'en',
+    langDir:'../app/internationalization',
+    locales: [
+      { code: 'en', name: 'English', file: 'en.json' },
+      { code: 'zh-CN', name: '简体中文', file: 'zh-CN.json' }
+    ]
   },
   colorMode: {
     classSuffix: '',
@@ -24,6 +32,7 @@ export default defineNuxtConfig({
     '@nuxtjs/color-mode',
     'shadcn-nuxt',
     '@nuxtjs/i18n',
-    '@nuxt/icon'
+    '@nuxt/icon',
+    '@nuxt/image'
   ]
 })
